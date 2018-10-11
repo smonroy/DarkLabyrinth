452f856f1fe65d90b9335b5012239a074873f846
Sergio Monroy Escalante <sergio.monroy.escalante@gmail.com>
2018-10-10 21:41:46 -0400

    class CharacterAction added but not finished

--- Assets/Scripts/CharacterAction.cs
+++ Assets/Scripts/CharacterAction.cs
@@ -3,5 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterAction {
+    public string name;
+    public int staminaCost;
+    public int range;
+    public EffectType effectType;
+    public ModifierType modifier;
+    public int modifierTurns;
 
+//    public CharacterAction(string name)
 }
