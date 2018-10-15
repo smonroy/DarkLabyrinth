ff6f50d48e5e129db60e747be2dab679540c81ce
Cheeseless <pedrobettt@gmail.com>
2018-10-14 23:12:38 -0400

    scraps

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public AudioSource leftAudioSource;
+    public AudioSource rightAudioSource;
+    public AudioSource centerAudioSource;
+    public AudioSource frontAudioSource;
+
+    public Object[] audioClips;
+
+    public void Load() {
+        SoundBank.LoadAudioDictionary();
+        foreach (KeyValuePair<string, AudioClip> keyValuePair in SoundBank.audioDictionary) {
+            centerAudioSource.clip = keyValuePair.Value;
+            centerAudioSource.Play();
+        }
+    }
+
+    void Start() {
+        Load();
+    }
+
+}
---
+++ Assets/Scripts/SoundBank.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class SoundBank {
+
+    public static Dictionary<String, AudioClip> audioDictionary;
+
+    public static void LoadAudioDictionary() {
+        Object[] audioClips;
+        audioDictionary = new Dictionary<string, AudioClip>();
+
+        audioClips = Resources.LoadAll("Audio", typeof(AudioClip));
+        foreach (Object audioClip in audioClips) {
+            audioDictionary.Add(audioClip.name, (AudioClip)audioClip);
+        }
+    }
+
+}
