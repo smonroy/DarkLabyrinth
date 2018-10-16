c2724f9e34204253975c67d50a2e2bcc9bd97726
Pedro Bento <pedrobettt@gmail.com>
2018-10-16 14:46:56 -0400

    starting queue method for audio manager

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,5 +1,6 @@
 ﻿#region usings
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,6 +19,32 @@ public class AudioManager : MonoBehaviour {
     public AudioSource musicAudioSource;
 
     AudioSource source;
+    Queue<AudioClip> audioQueue;
+    AudioPosition position;
+
+    public AudioPosition Position {
+        get { return position; }
+        set {
+            position = value;
+            switch (position) {
+                case AudioPosition.Left:
+                    source = leftAudioSource;
+                    break;
+                case AudioPosition.Center:
+                    source = centerAudioSource;
+                    break;
+                case AudioPosition.Right:
+                    source = rightAudioSource;
+                    break;
+                case AudioPosition.Front:
+                    source = frontAudioSource;
+                    break;
+                default:
+                    source = centerAudioSource;
+                    break;
+            }
+        }
+    }
 
     #endregion
 
@@ -31,29 +58,15 @@ public class AudioManager : MonoBehaviour {
 
     void Start() {
         Load();
+        audioQueue = new Queue<AudioClip>();
+        StartCoroutine(PlayQueue());
     }
 
     IEnumerator PlaySequenceCoroutine(string[] clips, AudioPosition position) {
-        switch (position) {
-            case AudioPosition.Left:
-                source = leftAudioSource;
-                break;
-            case AudioPosition.Center:
-                source = centerAudioSource;
-                break;
-            case AudioPosition.Right:
-                source = rightAudioSource;
-                break;
-            case AudioPosition.Front:
-                source = frontAudioSource;
-                break;
-            default:
-                source = centerAudioSource;
-                break;
-        }
         foreach (string clip in clips) {
-            source.clip = SoundBank.audioDictionary[clip];
-            source.Play();
+            source.clip = SoundBank.GetSound(clip);
+            if (source.clip != null)
+                source.Play();
             while (source.isPlaying) yield return null;
         }
     }
@@ -70,8 +83,51 @@ public class AudioManager : MonoBehaviour {
         }
     }
 
-    void PlaySequence(string[] clips, AudioPosition position = AudioPosition.Center) {
-        StartCoroutine(PlaySequenceCoroutine(clips, position));
+    void QueuePlay(string clips, AudioPosition pos, bool interrrupt = false) {
+        Position = pos;
+
+
+        List<string> playable = new List<string>(clips.Split(','));
+        foreach (string s in playable) SplitSpaces(s);
+        audioQueue.Enqueue(SoundBank.GetSound("space"));
+    }
+
+    void SplitSpaces(string s) {
+        List<string> thing = new List<string>(s.Split(' '));
+        foreach (string s1 in thing) {
+            int val;
+            if (Int32.TryParse(s1, out val)) {
+                ReadNumbers(val);
+            }
+            else {
+                audioQueue.Enqueue(SoundBank.GetSound(s1));
+                audioQueue.Enqueue(SoundBank.GetSound("space"));
+            }
+        }
+        audioQueue.Enqueue(SoundBank.GetSound("space"));
+    }
+
+    void ReadNumbers(int val) {
+        if (val <= 20) {
+            audioQueue.Enqueue(SoundBank.GetSound(val.ToString()));
+        }
+        else {
+            string valstring = val.ToString();
+            string tens, units;
+            if (valstring.Length == 3) {
+                string hundreds = valstring.Substring(0, 1) + "00";
+                tens = valstring.Substring(1, 1) + "0";
+                units = valstring.Substring(2, 1);
+                audioQueue.Enqueue(SoundBank.GetSound(hundreds));
+            }
+            else {
+                tens = valstring.Substring(0, 1) + "0";
+                units = valstring.Substring(1, 1);
+            }
+            audioQueue.Enqueue(SoundBank.GetSound(tens));
+            audioQueue.Enqueue(SoundBank.GetSound(units));
+        }
+        audioQueue.Enqueue(SoundBank.GetSound("space"));
     }
 
     void PlaySound(string clip, AudioPosition position = AudioPosition.Center) {
